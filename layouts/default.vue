94b75c4e864ae8480768bc1ec25499bdc0a2f9d4
farizge <farizghani11@gmail.com>
2023-10-03 14:49:13 +0700

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="app" class="flex flex-col min-h-screen">
+    <Navbar />
+    <Nuxt class="grow" />
+    <Footer />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <Hero />
+    <Brands />
+  </main>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
